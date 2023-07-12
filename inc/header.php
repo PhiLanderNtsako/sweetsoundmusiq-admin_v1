1e62061422d133e544a670e8364280878f863892
PhiLander <phil.ntsako98@gmail.com>
2023-07-12 11:25:36 +0200

    initial commit

---
+++ inc/header.php
@@ -0,0 +1,54 @@
+ <!-- Start Header Top Area -->
+    <div class="header-top-area">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+                    <div class="logo-area">
+                        <a href="index.php"><img src="img/logo/logo.png" alt="" /></a>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+    <!-- End Header Top Area -->
+    <!-- Mobile Menu start -->
+    <div class="mobile-menu-area">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                    <div class="mobile-menu">
+                        <nav id="dropdown">
+                            <ul class="mobile-menu-nav">
+                                <li><a href="artists.php">Artists</a></li>
+                                <li><a href="musiq.php">Musiq</a></li>
+                                <li><a href="playlists.php">Playlists</a></li>
+                                <li><a href="upload-musiq.php">Upload Musiq</a></li>
+                                <li><a href="add-artist.php">Add Artist</a></li>
+                                <li><a href="add-playlist.php">Add Playlist</a></li>
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Mobile Menu end -->
+      <!-- Main Menu area start-->
+    <div class="main-menu-area mg-tb-40">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                    <ul class="nav nav-tabs notika-menu-wrap menu-it-icon-pro">
+                        <li><a href="artists.php"><i class="notika-icon notika-support"></i> Artists</a></li>
+                        <li><a href="musiq.php"><i class="notika-icon notika-file"></i> Musiq</a></li>
+                        <li><a href="playlists.php"><i class="notika-icon notika-file"></i> Playlists</a></li>
+                        <li><a href="upload-musiq.php"><i class="notika-icon notika-edit"></i> Upload Musiq</a></li>
+                        <li><a href="add-artist.php"><i class="notika-icon notika-edit"></i>Add Artist</a></li>
+                        <li><a href="add-playlist.php"><i class="notika-icon notika-edit"></i>Add Playlist</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Main Menu area End-->
